0c85e2d63fd5af89a2e31daa84a76f6f56581ea0
Giassinne <yassinekaddouri2711@gmail.com>
2025-05-07 23:43:14 +0200

    Affichage des pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet,RouterModule } from '@angular/router';
 import { FormatTextPipe } from './pipes/format.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, FormatTextPipe],
+  imports: [RouterLink, RouterOutlet, FormatTextPipe,RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
